e85bbcfa847842572459415a9c7bc3e2badac01a
Natdanai-Aud <natdanai.aud@ku.th>
2025-08-17 22:09:40 +0700

    Add files via upload
    Signed-off-by: Natdanai-Aud <natdanai.aud@ku.th>

--- lab6.cpp
+++ lab6.cpp
@@ -2,36 +2,36 @@
 int main()
 {
 //1.1
-/*
+	
 	int k;
 	for(k=0;k>=-56;k--)
 		printf("%d\n",k);
-*/
+	
 //1.2
-/*
+	/*
 	int k;
 	for(k=1;k<=10;k++)
 		printf("%d\n",k*8);
-*/
+	*/
 //ex.1.2
-/*
+	/*
 	int i,N;
 	printf("Enter N: "); scanf("%d",&N);
 	for(i=1;i<=N;i++){
 		if(N%i==0) printf("%d\n",i);
 	}
-*/
+	*/
 //1.3
-/*
+	/*
 	int i,N,count=0;
 	printf("Enter N: "); scanf("%d",&N);
 	for(i=1;i<=N;i++){
 		if(N%i==0)count=count+1;
 	}
 	printf("The number of divisors of %d is %d.\n",N,count);
-*/
+	*/
 //1.4
-/*
+	/*
 	int i,a,b,count=0;
 	printf("Input A = "); scanf("%d",&a);
 	printf("Input B = "); scanf("%d",&b);
@@ -39,12 +39,12 @@ int main()
 		if(i%7==0){
 		count=count+1;
 		printf("%d ",i);
-	}
+		}
 	}
 	printf("\nOutput : There is %d number.",count);
-*/
+	*/
 //1.5
-/*
+	/*
 	int i,a,b,c,count=0;
 	printf("Input A = "); scanf("%d",&a);
 	printf("Input B = "); scanf("%d",&b);
@@ -53,12 +53,12 @@ int main()
 		if(i%c==0){
 		count=count+1;
 		printf("%d ",i);
-	}
+		}
 	}
 	printf("\nOutput : There is %d number divisible by %d.",count,c);
-*/
+	*/
 //1.6
-/*
+	/*
 	int i,N,count=0;
 	printf("Enter N: "); scanf("%d",&N);
 	for(i=1;i<=N;i++){
@@ -69,51 +69,71 @@ int main()
 			printf("%d is a prime.",N);
 		else
 			printf("%d is not a prime.",N);
-*/
+	*/
 //2.1
-/*
+	/*
 	int n,i,j;
 	printf("Input n : "); scanf("%d",&n);
-	for(i=n;i>0;i--)
-	{
-		for(j=n;j>1;j--)
-		{
+	for(i=n;i>0;i--){
+		for(j=n;j>1;j--){
 			printf("*");
 		}
 		printf("*\n");
-			}
-*/
+	}
+	*/
 //2.2
-/*
-int n,i,j;
+	/*
+	int n,i,j;
 	printf("Input n : "); scanf("%d",&n);
-	for(i=n;i>=0;i--)
-	{
-		for(j=n;j>i;j--)
-		{
+	for(i=n;i>=0;i--){
+		for(j=n;j>i;j--){
 			printf("*");
 		}
 		printf("\n");
-			}
-*/
-//-2.3-
-/*
-int n,i,j;
+	}
+	*/
+//2.3
+	/*
+	int n,i,j;
 	printf("Input n : "); scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
+	for(i=1;i<=n;i++){
 		for(j=1;j<=n-i;j++) printf(" ");
 		for(j=1;j<=2*i-1;j++)
 		printf("*");
 		printf("\n");
-			}
-*/
-	int n,i;
-	printf("Please input N : "); scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	if(i%2==0){
-		printf("Factor of n : %d\n",i);
 	}
-	
-	printf("Sumation of factor is %d");
+	*/
+//3.1
+	/*
+	int n,sum=0,i;
+	printf("Please input N : "); scanf("%d",&n);
+	printf("Factor of n : ");
+	for(i=1;i<=n;i++){
+		if(n%i==0){
+			printf("%d ",i);
+			sum+=i;
+		}
+	}
+	printf("\nSumation of factor is %d.",sum);
+	*/
+//3.2
+	/*
+	int n,sum=0,i;
+	while(n>=0){
+		sum=0;
+		printf("Please input N : "); scanf("%d",&n);
+		if(n>=0){
+			printf("Factor of n : ");
+			for(i=1;i<=n;i++){
+				if(n%i==0){
+					printf("%d ",i);
+					sum+=i;
+				}
+			}
+		printf("\nSumation of factor is %d.\n\n",sum);
+		}
+	}
+	printf("\n");
+	printf("Program End.");
+	*/
 }
